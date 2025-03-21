1e89bb4027a916a8e716420ff53a41b0195458ee
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-03-21 17:03:58 +0100

    game

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,38 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
     private int currentTurn;
-    private int numberOfTurns;
+    private final int numberOfTurns;
+    private int activePlayerIndex;
     private Player activePlayer;
+    private final List<Player> players;
+    private final Map playField;
 
-    public void nextPlayer() {}
-    public void start() {}
-    public void nextTurn() {}
-    public void end() {}
+    public Game(List<Player> playersInput) {
+        activePlayerIndex = 0;
+        currentTurn = 0;
+        playField = new Map();
+        numberOfTurns = 50;
+        players = playersInput;
+    }
+
+    public void nextPlayer() {
+        activePlayerIndex++;
+        //activePlayer = players[activePlayerIndex];
+    }
+    public void start() {
+        //activePlayer = players[activePlayerIndex];
+        playField.generate();
+    }
+    public void nextTurn() {
+        activePlayerIndex = 0;
+        currentTurn++;
+        if(currentTurn == numberOfTurns) {
+            end();
+        }
+    }
+    public void end() {
+        System.out.println("Game ended");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private Game game;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Szia tesztelo");
     }
+
+    public void initTest() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Gombasz jatekos neve: ");
+        String playerName1 = scanner.nextLine();
+        System.out.println("Rovarasz jatekos neve: ");
+        String playerName2 = scanner.nextLine();
+
+        MushroomPicker mp = new MushroomPicker(playerName1, new Mushroom());
+        Entomologist el = new Entomologist(playerName2, new Insect());
+
+        List<Player> pl = new ArrayList<>();
+        pl.add(mp);
+        pl.add(el);
+        game = new Game(pl);
+        game.start();
+    }
+
+    public void teszt1() {
+
+    }
+
+    public void teszt2() {
+
+    }
+
+    public void teszt3() {
+
+    }
+
+    public void teszt4() {
+
+    }
+
+    public void teszt5() {
+
+    }
+
+    public void teszt6() {
+
+    }
+
+    public void teszt7() {
+
+    }
+
+    public void teszt8() {
+
+    }
+
+    public void teszt9() {
+
+    }
+
+    public void teszt10() {
+
+    }
+
+
+
+
 }
