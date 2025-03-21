b2132bb9c2c63280100190ca15e3d498d159bebf
ARMAND VÖRÖS <armandvoros20@gmail.com>
2025-03-21 14:44:29 +0100

    YarnAbsorbantTecton

--- src/YarnAbsorbantTecton.java
+++ src/YarnAbsorbantTecton.java
@@ -1,5 +1,25 @@
+/** A YarnAbsorbantTecton class egy speciális Tecton, ami felszívja a gombafonalakat. */
 public class YarnAbsorbantTecton extends Tecton {
-    @Override
-    public void runEffectYarn(Yarn yarn) {}
-}
 
+    /**
+     * A YarnAbsorbantTecton class konstruktora.
+     *
+     * @param yarnLimit A maximális gombafonal szám, ami a Tectonon lehet.
+     * @param mushroomPrevent A  jelzés, hogy a Tectonon nem lehet gomba.
+     */
+    public YarnAbsorbantTecton(int yarnLimit, boolean mushroomPrevent) {
+        super(yarnLimit, mushroomPrevent);
+    }
+
+    /**
+     * Híváskor eltávolítja magáról a gombafonalat.
+     *
+     * @param yarn A felszívandó gombafonal.
+     */
+    @Override
+    public void runEffect(Yarn yarn) {
+        System.out.println("Running absorbant effect on yarn: " + yarn);
+        yarn.getTectons().remove(this);
+        removeYarn(yarn);
+    }
+}
