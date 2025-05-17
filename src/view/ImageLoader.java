70110c7fa81b897896c994261372cd19d2093792
Dózsa Domonkos <dozsa.domonkos@gmail.com>
2025-05-17 19:20:34 +0200

    load-fix-for-vm

--- src/view/ImageLoader.java
+++ src/view/ImageLoader.java
@@ -1,14 +1,14 @@
 package view;
-
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
+import java.io.File;
 
 /**
  * Segédosztály képek betöltésére és átméretezésére.
  * Ez az osztály egyszerűsíti a képek betöltését és átméretezését a program különböző komponensei számára.
  */
 public class ImageLoader {
-
     /**
      * Betölt egy képet a megadott elérési útról és átméretezi a megadott méretekre.
      *
@@ -18,14 +18,52 @@ public class ImageLoader {
      * @return Az átméretezett képet tartalmazó ImageIcon
      */
     public static ImageIcon load(String path, int width, int height) {
-        // Kép betöltése
-        ImageIcon icon = new ImageIcon(ImageLoader.class.getResource(path));
+        try {
+            // Kérjük le a projekt könyvtárat
+            String projectDir = System.getProperty("user.dir");
 
-        // Kép átméretezése
-        Image img = icon.getImage();
-        Image newImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            // Tisztítsuk meg az elérési útvonalat
+            String cleanPath = path;
+            if (cleanPath.startsWith("/")) {
+                cleanPath = cleanPath.substring(1);
+            }
 
-        // Új ImageIcon létrehozása az átméretezett képből
-        return new ImageIcon(newImg);
+            // Hozzuk létre a teljes elérési utat
+            File imageFile = new File(projectDir + "/src/" + cleanPath);
+
+            // Ellenőrizzük, hogy létezik-e a fájl
+            if (!imageFile.exists()) {
+                System.err.println("Nem található kép: " + imageFile.getAbsolutePath());
+                return createPlaceholder(width, height);
+            }
+
+            // Kép betöltése
+            ImageIcon icon = new ImageIcon(imageFile.getAbsolutePath());
+
+            // Kép átméretezése
+            Image img = icon.getImage();
+            Image newImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+            // Új ImageIcon létrehozása az átméretezett képből
+            return new ImageIcon(newImg);
+        } catch (Exception e) {
+            System.err.println("Hiba a kép betöltése során: " + path);
+            e.printStackTrace();
+            return createPlaceholder(width, height);
+        }
+    }
+
+    /**
+     * Létrehoz egy üres placeholder képet hiba esetére
+     */
+    private static ImageIcon createPlaceholder(int width, int height) {
+        BufferedImage placeholder = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        // Rajzoljunk egy egyszerű "X"-et a placeholderre
+        java.awt.Graphics2D g2d = placeholder.createGraphics();
+        g2d.setColor(java.awt.Color.RED);
+        g2d.drawLine(0, 0, width, height);
+        g2d.drawLine(0, height, width, 0);
+        g2d.dispose();
+        return new ImageIcon(placeholder);
     }
 }
